d872028d95c5310054b761fcecd55446b19caa29
Nagraju1418 <124027796+Nagraju1418@users.noreply.github.com>
2023-02-02 18:47:55 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class Animal {
+    public void eat(){
+        System.out.println("Eats food");
+    }
+    public void sleep(){
+        System.out.println("Animals need rest");
+    }
+}
